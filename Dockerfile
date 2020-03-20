be3c738643e8dcf47a60a108a92a9caef0e5213a
rotinov <rotinov-github@pm.me>
2020-03-20 11:06:34 +0300

    Init

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM docker:19.03.1 as runtime
+LABEL "com.github.actions.name"="Build&Publish Docker"
+LABEL "com.github.actions.description"="Uses the git branch as the docker tag and pushes the container"
+LABEL "com.github.actions.icon"="anchor"
+LABEL "com.github.actions.color"="blue"
+
+LABEL "repository"="https://github.com/rotinov/Build-n-Publish-Docker/"
+LABEL "maintainer"="Rotinov Egor"
+
+RUN apk update \
+  && apk upgrade \
+  && apk add --no-cache git
+
+ADD entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
+
+FROM runtime
---
+++ entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+
+BRANCH=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g")
+
+if [ "${BRANCH}" == "master" ]; then
+  BRANCH="latest"
+fi;
+
+# if contains /refs/tags/
+if [ $(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g") != ${GITHUB_REF} ]; then
+  BRANCH="latest"
+fi;
+
+echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin ${INPUT_REGISTRY}
+
+DOCKERNAME="${INPUT_NAME}:${BRANCH}"
+BUILDPARAMS="${INPUT_EXTRA_BUILD_PARAMS}"
+
+if [ ! -z "${INPUT_DOCKERNAME_APPENDIX}" ]; then
+  DOCKERNAME="$DOCKERNAME${INPUT_DOCKERNAME_APPENDIX}"
+fi
+
+if [ ! -z "${INPUT_DOCKERFILE}" ]; then
+  BUILDPARAMS="$BUILDPARAMS -f ${INPUT_DOCKERFILE}"
+fi
+
+if [ ! -z "${INPUT_CACHE}" ]; then
+  docker pull ${DOCKERNAME}
+  BUILDPARAMS="$BUILDPARAMS --cache-from ${DOCKERNAME}"
+fi
+
+
+DOCKER_TAG=$(echo ${GITHUB_REF} | sed -e 's/refs\/tags\/v//')
+docker build $BUILDPARAMS -t ${INPUT_NAME}:latest -t ${INPUT_NAME}:${DOCKER_TAG} .
+docker push ${INPUT_NAME}:latest
+docker push ${INPUT_NAME}:${DOCKER_TAG}
+
+docker logout
